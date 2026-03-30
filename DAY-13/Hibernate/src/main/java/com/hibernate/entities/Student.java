e70c8cfe0468a7dcd2300861b00edba68e95444c
anujxp <ganuj293@gmail.com>
2026-03-30 23:57:39 +0530

    Hibernate(xml configuration): implement hibernate

---
+++ DAY-13/Hibernate/src/main/java/com/hibernate/entities/Student.java
@@ -0,0 +1,36 @@
+package com.hibernate;
+
+public class Student {
+    private int id;
+    private String name;
+    private String city;
+
+    // Default Constructor, Getters and Setters are MANDATORY
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    // ... add name and city getters/setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ DAY-13/hibernate-annotation-practical/src/main/java/com/hibernate/Student.java
@@ -0,0 +1,4 @@
+package com.hibernate;
+
+public class Student {
+}
